30364f5e3387bdbbb8be2879d30c1986ed48facb
황광회 <jack7141@nate.com>
2019-03-28 16:48:06 +0900

    first

---
+++ main.cpp
@@ -0,0 +1,60 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
+Mat  Image ;
+
+int main()
+{
+    Image = imread("C:/Users/ksrnd/Desktop/Light/20190326_004.png");
+    imshow("Original",Image);
+    Mat img_gray;
+    cvtColor(Image,img_gray,CV_RGB2GRAY);
+    imshow("img_gray",img_gray);
+    Mat mask;
+    blur(img_gray, mask, Size(3, 3));
+    Canny(img_gray, mask, 70, 150, 3);
+    imshow("Canny",mask);
+    vector<vector<Point>> contours;
+
+    Mat element = getStructuringElement(MORPH_RECT,Size(3,3));
+    morphologyEx(mask,mask,MORPH_CLOSE,element,Point(-1,-1),10);
+    erode(mask, mask, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+    erode(mask, mask, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+    imshow("morphologyEx",mask);
+    findContours( mask, contours, CV_RETR_EXTERNAL, CHAIN_APPROX_SIMPLE );
+    vector<vector<Point>> contours_poly(contours.size());
+    vector<Rect> boundRect(contours.size());
+
+    for ( size_t i=0; i<contours.size(); i++) {
+
+        approxPolyDP(contours[i],contours_poly[i],3,true);
+        boundRect[i]=boundingRect(contours_poly[i]);
+
+    }
+    vector<Point> count;
+    for ( size_t i=0; i<contours.size(); i++ ) {
+
+        //Center Point
+        Point centerRect = (boundRect[i].tl() + boundRect[i].br())*0.5;
+        count.push_back(centerRect);
+        if ( count.size() > 2 ) {
+            if ( -(boundRect[i].x - boundRect[i+1].x) >15 || -(boundRect[i].x - boundRect[i+1].x) < 100 )
+            {
+                centerRect = (boundRect[i].tl() + boundRect[i].br())*0.5;
+                count.pop_back();
+            }
+
+        }
+        for ( size_t i=0; i<count.size(); i++ ) {
+
+            rectangle( Image, boundRect[i].tl(), boundRect[i].br(), Scalar(255,255,0), 2);
+            Point centerRect = (boundRect[i].tl() + boundRect[i].br())*0.5;
+            circle(Image,centerRect,3,Scalar(255,0,255));
+        }
+
+    }
+
+    imshow("Image",Image);
+    waitKey();
+}
