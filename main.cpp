a66a6ed4bae44b0668f6f42f311c7a98c992d75e
황광회 <jack7141@nate.com>
2019-03-28 17:26:41 +0900

    sec

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main()
     findContours( mask, contours, CV_RETR_EXTERNAL, CHAIN_APPROX_SIMPLE );
     vector<vector<Point>> contours_poly(contours.size());
     vector<Rect> boundRect(contours.size());
-
+    Point centerRect;
     for ( size_t i=0; i<contours.size(); i++) {
 
         approxPolyDP(contours[i],contours_poly[i],3,true);
@@ -33,20 +33,26 @@ int main()
 
     }
     vector<Point> count;
+    vector<Point> count2;
     for ( size_t i=0; i<contours.size(); i++ ) {
 
         //Center Point
-        Point centerRect = (boundRect[i].tl() + boundRect[i].br())*0.5;
+        centerRect = (boundRect[i].tl() + boundRect[i].br())*0.5;
         count.push_back(centerRect);
-        if ( count.size() > 2 ) {
-            if ( -(boundRect[i].x - boundRect[i+1].x) >15 || -(boundRect[i].x - boundRect[i+1].x) < 100 )
+    }
+
+    if ( count.size() > 2 ) {
+        for ( size_t i=0; i<count.size(); i++) {
+            if ( -(boundRect[i].x - boundRect[i+1].x) > 15 && -(boundRect[i].x - boundRect[i+1].x) < 130 )
             {
                 centerRect = (boundRect[i].tl() + boundRect[i].br())*0.5;
-                count.pop_back();
+                cout << centerRect << endl;
+                count2.push_back(centerRect);
+                       cout << count2 << endl;
             }
-
         }
-        for ( size_t i=0; i<count.size(); i++ ) {
+
+        for ( size_t i=0; i<count2.size(); i++ ) {
 
             rectangle( Image, boundRect[i].tl(), boundRect[i].br(), Scalar(255,255,0), 2);
             Point centerRect = (boundRect[i].tl() + boundRect[i].br())*0.5;
